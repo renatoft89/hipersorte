0fab85026f14375597c329ab7f8eb91b11d7ce89
renatoft89 <renatoft89@gmail.com>
2024-11-29 21:58:24 -0300

    fix: update scrapping

--- back/src/getApi/index.js
+++ back/src/getApi/index.js
@@ -4,22 +4,24 @@ const resultMega = async () => {
   try {
     const browser = await puppeteer.launch({
       executablePath: '/usr/bin/chromium-browser',
-      headless:false,
+      headless: false,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
     const page = await browser.newPage();
-    await page.goto('https://loterias.caixa.gov.br/Paginas/default.aspx');
+    await page.goto('https://loterias.caixa.gov.br/Paginas/default.aspx', { waitUntil: 'networkidle2' });
+
+    await page.waitForSelector('ul.resultado-loteria.mega-sena li');
+    await page.waitForSelector('ul.simple-container.lista-dezenas.lotofacil li.ng-binding.dezena.ng-scope');
+    await page.waitForSelector('ul.simple-container.lista-dezenas.quina li.ng-binding.dezena.ng-scope');
 
     const lotoResults = await page.evaluate(() => {
-      const mega = Array.from(document.querySelectorAll('ul.resultado-loteria.mega-sena li')).map(li => li.innerText);
-      const lotofacil = Array.from(document.querySelectorAll('ul.simple-container.lista-dezenas.lotofacil li.ng-binding.dezena.ng-scope')).map(li => li.innerText);
-      const quina = Array.from(document.querySelectorAll('ul.simple-container.lista-dezenas.quina li.ng-binding.dezena.ng-scope')).map(li => li.innerText);
-      
+      const getNumbers = (selector) => Array.from(document.querySelectorAll(selector)).map(li => li.innerText);
+
       return {
-        mega,
-        lotofacil,
-        quina
+        mega: getNumbers('ul.resultado-loteria.mega-sena li'),
+        lotofacil: getNumbers('ul.simple-container.lista-dezenas.lotofacil li.ng-binding.dezena.ng-scope'),
+        quina: getNumbers('ul.simple-container.lista-dezenas.quina li.ng-binding.dezena.ng-scope'),
       };
     });
 
